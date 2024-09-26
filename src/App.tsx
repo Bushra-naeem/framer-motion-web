dc5a1cec620dc4fe8aeb5141232498e4d018a930
Bushra <122994098+Bushra-naeem@users.noreply.github.com>
2024-09-27 01:03:07 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
     <SmoothScroll>
       <MovingCursor>
         <Header />
-        <Suspense fallback={<div className='ml-4'>Loading...</div>}>
+        <Suspense fallback={<div className='flex items-center justify-center ml-4'>Loading...</div>}>
         <AnimatePresence mode='wait'>
           <Routes location={location} key={location.pathname}>
             <Route path="/" element={<Home />} />
